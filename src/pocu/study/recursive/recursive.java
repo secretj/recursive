b55ac083fb5deee7789435c63e5a24e1a0e23d6a
secretj <qkrwlsgud890@gmail.com>
2022-01-09 01:05:55 +0900

    전체 합구하기 꼬리 재귀

--- src/pocu/study/recursive/recursive.java
+++ src/pocu/study/recursive/recursive.java
@@ -4,14 +4,25 @@ public class recursive {
 
 	public static void main(String[] args) {
 		System.out.println(fibonacci(5));
+		System.out.println(sumTailRecursive(100000,0));
 		
 
 	}
+	//피보나치 수열
 	public static int fibonacci(int n) {
 		if(n<=1) {
 			return n;
 		}
 			return fibonacci(n-1)+fibonacci(n-2);
+	}
+	
+	// 전체 수의 합을 구하는 꼬리 재귀함수
+	public static int sumTailRecursive(int n, int sum) {
+		
+		if(n<=0) {
+			return sum;
+		}
+		return sumTailRecursive(n-1, sum+n); 
 	}
 
 }

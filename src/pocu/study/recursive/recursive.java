469a5da1a0a49f993741d4c5edea8745270d835d
secretj <qkrwlsgud890@gmail.com>
2022-01-09 04:24:14 +0900

    이진 탐색 알고리듬

--- src/pocu/study/recursive/recursive.java
+++ src/pocu/study/recursive/recursive.java
@@ -4,7 +4,7 @@ public class recursive {
 
 	public static void main(String[] args) {
 		System.out.println(fibonacci(5));
-		System.out.println(sumTailRecursive(100000,0));
+		System.out.println(sumTailRecursive(1000,0));
 		
 
 	}
@@ -24,5 +24,35 @@ public class recursive {
 		}
 		return sumTailRecursive(n-1, sum+n); 
 	}
+	
+	//반복문으로 만든 이진 탐색 알고리듬 
+	public static int binarySearchRecursive(int arr[], int target) {
+	    int low = 0;
+	    int high = arr.length - 1;
+	    int mid;
 
-}
+	    while(low <= high) {
+	        mid = (low + high) / 2;
+
+	        if (arr[mid] == target)
+	            return mid;
+	        else if (arr[mid] > target)
+	            high = mid - 1;
+	        else
+	            low = mid + 1;
+	    }
+	    return -1;			//array안에서 숫자를 찾지 못했단 의미 
+	}
+	
+	// 재귀함수를 이용해서 만든 이진 탐색 알고리듬
+	public static int binarySearchRecursive2(int nums[], int value, int low, int high) {
+	    if (low > high)
+	        return -1;			//array안에서 숫자를 찾지 못했단 의미 
+	    int mid = (low + high) / 2;
+	    if (nums[mid] == value)
+	        return mid;
+	    else if (nums[mid] > value)
+	        return binarySearchRecursive2(nums, value, low, mid-1);
+	    else
+	        return binarySearchRecursive2(nums, value, mid+1, high);
+	}
